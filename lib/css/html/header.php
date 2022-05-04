93f9726c82f505e27cc06f1df5c4e8063ad5fc50
Wendel Pedro <72534930+WendelF03@users.noreply.github.com>
2022-05-04 00:16:00 -0300

    Add files via upload
    add html

---
+++ lib/css/html/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Calendário</title>
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'../../lib/js/FullCalendar/main.min.css';?>">
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'../../lib/css/style.css';?>">
+</head>
+<body>
